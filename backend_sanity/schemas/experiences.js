4d5f8d55c6b3e7ee4e37e1f429ac0906955eecbb
IoannisTzotze <john.xoxe@gmail.com>
2022-07-05 15:09:38 +0100

    V1.0 of New Portfolio

---
+++ backend_sanity/schemas/experiences.js
@@ -0,0 +1,22 @@
+
+// Sort below code to dispaly in ascending order of year from oldest to newest
+
+export default{
+
+    name:'experiences',
+    title:'Experiences',
+    type: 'document',
+    fields:[
+        {
+            name:'year',
+            title:'Year',
+            type:'string'
+        },
+        {
+            name:'works',
+            title:'Works',
+            type:'array',
+            of:[{ type:'workExperience'}]
+        },
+    ],
+}
